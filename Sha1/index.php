a43947aa644fdeffd1cf55823230198d23edb626
misterfreezes <aureliencabirol@gmail.com>
2016-02-04 11:33:13 +0100

    Added Sha1 cryptage

---
+++ Sha1/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Hashage Sha1</title>
+		<meta charset='utf-8'/> 
+	</head>
+	<body>
+		<form action="index.php" method="post">
+			<input type="text" name="crypt" />
+			<input type="submit" value="Crypter !"/>
+		</form>
+
+		<p>La chaîne de caractère crypté est :</p>
+			<?php
+				//initialisation du hashage //ajout commentaire
+				if (isset($_POST['crypt'])) {
+					
+					echo '<p>' . sha1($_POST['crypt']) . '</p>';
+				}
+			?>
+	</body>
+</html>
