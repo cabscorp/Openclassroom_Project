eb09a734bd52a845547cc3084b5dc3955cb8de77
misterfreezes <aureliencabirol@gmail.com>
2016-02-08 22:24:20 +0100

    Added md5 hash | Modify input name

--- Sha1/index.php
+++ Sha1/index.php
@@ -6,15 +6,28 @@
 	</head>
 	<body>
 		<form action="index.php" method="post">
-			<input type="text" name="crypt" />
+			<input type="text" name="cryptsha1" />
 			<input type="submit" value="Crypter !"/>
 		</form>
 
 		<p>Le hash Sha1 est :</p>
 			<?php
-				if (isset($_POST['crypt']) && $_POST['crypt']) {
+				if (isset($_POST['cryptsha1']) && $_POST['cryptsha1']) {
 					
-					echo '<p>' . sha1($_POST['crypt']) . '</p>';
+					echo '<p>' . sha1($_POST['cryptsha1']) . '</p>';
+				}
+			?>
+
+		<form action="index.php" method="post">
+			<input type="text" name="cryptmd5" />
+			<input type="submit" value="Crypter !"/>
+		</form>
+
+		<p>Le hash Md5 est :</p>
+			<?php
+				if (isset($_POST['cryptmd5']) && $_POST['cryptmd5']) {
+					
+					echo '<p>' . md5($_POST['cryptmd5']) . '</p>';
 				}
 			?>
 	</body>
