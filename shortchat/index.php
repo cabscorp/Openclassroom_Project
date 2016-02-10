70bae3a2b9fb6e7849b143260f02e8ddb227f54b
misterfreezes <aureliencabirol@gmail.com>
2016-02-10 18:00:36 +0100

    This reverts commit 2f7b66223bf2e8055a0f718096484abf4fd17117.

--- shortchat/index.php
+++ shortchat/index.php
@@ -39,14 +39,14 @@ session_start();
 		        die('Erreur : '.$e->getMessage());
 		}
 
-		$reponse = $bdd->query('SELECT pseudo, message, DATE_FORMAT(date, \'%d/%m/%Y %H:%i\') AS date FROM minichat ORDER BY ID DESC LIMIT 0, 10');
+		$reponse = $bdd->query('SELECT pseudo, message, date FROM minichat ORDER BY ID DESC LIMIT 0, 10');
 		
 
 		while ($donnees = $reponse->fetch())
 		{
 		?>
 			<p>
-				<?php echo '<b>' . htmlspecialchars($donnees['pseudo']) . '</b>' . ' : ' . htmlspecialchars($donnees['message']) . ' (' . $donnees['date'] . ')'; ?>
+				<?php echo '<b>' . htmlspecialchars($donnees['pseudo']) . '</b>' . ' : ' . htmlspecialchars($donnees['message']) . " le " . $donnees['date']; ?>
 			</p>
 		<?php	
 			
