ff91fb20071845afe43905c84101efd9d610f20f
misterfreezes <aureliencabirol@gmail.com>
2016-02-11 13:02:38 +0100

    Display billets on index, modified style

--- Blog/index.php
+++ Blog/index.php
@@ -2,9 +2,39 @@
 <html>
 	<head>
 		<title>Blog</title>
-		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<meta charset="utf-8" />
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
 	</head>
 	<body>
+		<?php
+		try
+		{
+			$bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '', 
+			array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+		}
+		catch(Exception $e)
+		{
+		        die('Erreur : '.$e->getMessage());
+		}
 
+		$reponse = $bdd->query('SELECT title, content, DATE_FORMAT(date_creation, \'%d/%m/%Y %H:%i\') AS date FROM billets ORDER BY date DESC LIMIT 0, 5');
+		
+
+		while ($donnees = $reponse->fetch())
+		{
+		?>
+			<h3><?php echo $donnees['title']; ?></h3>
+			
+			<div class="news">
+				<p><?php echo $donnees['content']; ?></p>
+			</div>
+
+		<?php	
+			
+		}	
+
+		$reponse->closeCursor();
+
+		?>
 	</body>
 </html>
