a7981ceb63dff6ce963f59e66cf75d6ecd4125a0
misterfreezes <aureliencabirol@gmail.com>
2016-02-21 21:48:59 +0100

    Xss protection

--- Blog/commentary.php
+++ Blog/commentary.php
@@ -13,6 +13,8 @@ catch(Exception $e) {
 $req = $bdd->prepare('SELECT id, title, content, DATE_FORMAT(date_creation, \'%d/%m/%Y %H:%i\') AS date FROM billets WHERE id = ?');
 $req->execute(array($_GET['billet']));
 $donnees = $req->fetch();
+//Close first request
+$req->closeCursor();
 
 ?>
 <!DOCTYPE html>
@@ -27,49 +29,52 @@ $donnees = $req->fetch();
         <div id="content">
             <section>
                 <?php
+                
                 //See if billet exist
-                if(!empty($donnees)){
+                if (!empty($donnees)) {
                     
                     //Display billet
                     include('billet_commentary.php');
 
-                //Close first request
-                $req->closeCursor();
-                
-                //Open second request (Display commentary)
-                echo '<hr /><h2>Commentaires</h2>';
+                    //Open second request (Display commentary)
+                    echo '<hr /><h2>Commentaires</h2>';
 
-                } else {
                     
-                    echo "<p>Ce billet n'existe pas.</p>";
-                }
-                
-                $req = $bdd->prepare('SELECT autor, content, DATE_FORMAT(date_commentary, \'%d/%m/%Y à %H:%i\') AS date FROM commentary WHERE id_billet = ?');
-                $req->execute(array($_GET['billet']));
-                
-                while($donnees = $req->fetch()) {
-                ?>
+                    $req = $bdd->prepare('SELECT autor, content, DATE_FORMAT(date_commentary, \'%d/%m/%Y à %H:%i\') AS date FROM commentary WHERE id_billet = ?');
+                    $req->execute(array($_GET['billet']));
 
-                <h4><?php echo htmlspecialchars($donnees['autor']) . ' ' . $donnees['date']; ?></h4>
-                <p><?php echo htmlspecialchars($donnees['content']); ?></p>
+                    while($donnees = $req->fetch()) {
+                    ?>
 
-                <?php
-                
-                }
-                
-                $req->closeCursor();
-                ?>
+                        <h4><?php echo htmlspecialchars($donnees['autor']) . ' ' . $donnees['date']; ?></h4>
+                        <p><?php echo htmlspecialchars($donnees['content']); ?></p>
 
-                <br />
+                    <?php
+                    } 
+                    ?>
+                     
+                     <br />
 
-                <form action="commentary_post.php" method="post">
-                    <label for="auteur">Nom : </label><input type="text" name="auteur" id="auteur" /><br /><br />
-                    <label for="commentary_new" id="labelcommentary">Poster un commentaire :</label><br />
-                    <textarea name="commentary_new" id="commentary_new" rows="8" cols="50"></textarea><br />
-                    <input type='hidden' name="billet_id" value='<?php echo $_GET['billet'] ?>' >
-                    <input type="submit" value="Envoyer" />
-                </form>
+                    <form action="commentary_post.php" method="post">
+                        <label for="auteur">Nom : </label><input type="text" name="auteur" id="auteur" /><br /><br />
+                        <label for="commentary_new" id="labelcommentary">Poster un commentaire :</label><br />
+                        <textarea name="commentary_new" id="commentary_new" rows="8" cols="50"></textarea><br />
+                        <input type='hidden' name="billet_id" value='<?php echo $_GET['billet'] ?>' >
+                        <input type="submit" value="Envoyer" />
+                    </form>
+
+                    <?php
+                    
+                    } else {
+                        
+                        echo "<h4>Ce billet n'existe pas.</h4>";
+                    }
+                    
+                    $req->closeCursor();
+
+                    ?>
                 <p><a href="index.php">Retour à la liste des billets</a></p>
+            
             </section>
         </div>
     </body>
--- Blog/commentary_post.php
+++ Blog/commentary_post.php
@@ -11,10 +11,22 @@ catch(Exception $e) {
     die('Erreur : '.$e->getMessage());
 }
 
-$req = $bdd->prepare('INSERT INTO commentary(id_billet, autor, content, date_commentary) VALUES (:id_billet, :autor, :content, NOW())');
-$req->execute(array('id_billet' => $_POST['billet_id'], 'autor' => $_POST['auteur'], 'content' => $_POST['commentary_new']));
-$req->closeCursor();
-$i = $_POST['billet_id'];
+if (!empty($_POST['auteur'] AND $_POST['commentary_new'])) {
+	
+	$req = $bdd->prepare('INSERT INTO commentary(id_billet, autor, content, date_commentary) VALUES (:id_billet, :autor, :content, NOW())');
+	$req->execute(array('id_billet' => $_POST['billet_id'], 'autor' => $_POST['auteur'], 'content' => $_POST['commentary_new']));
+	$req->closeCursor();
 
-header('Location: commentary.php?billet=' . $_POST['billet_id']);
+	header('Location: commentary.php?billet=' . $_POST['billet_id']);
+
+} else {
+?>
+
+<script type="text/javascript">
+	alert('Veuillez rentrer un nom et un commentaire !');
+</script>
+
+<?php
+}
+
 ?>
