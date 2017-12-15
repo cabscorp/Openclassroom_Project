83a598a88afc9d408e2b8b2d7a70bbc270725896
CabsCorp <aureliencabirol@gmail.com>
2017-12-15 19:22:23 +0100

    htmlspecialchars to strip_tag

--- Blog/billet.php
+++ Blog/billet.php
@@ -1,7 +1,7 @@
-<h3><?php echo htmlspecialchars($donnees['title']) . ' ' . $donnees['date']; ?></h3>
+<h3><?php echo strip_tags($donnees['title']) . ' ' . $donnees['date']; ?></h3>
 <div class="news">
 	<p>	
-		<?php echo nl2br(htmlspecialchars($donnees['content'])); ?>
+		<?php echo nl2br(strip_tags($donnees['content'])); ?>
 		<br />
 		<a href="commentary.php?billet=<?php echo $donnees['id']; ?>">Commentaires</a>
 	</p>
